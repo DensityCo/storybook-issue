58b3126f83dbdbc818db67aebae904b4ec45b273
Ryan Gaus <rsg1egoman@gmail.com>
2017-10-09 11:30:34 -0400

    chore: remove details for trying to get help with issue

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,12 +1,7 @@
-import { configure, setAddon } from '@storybook/react';
+import { configure } from '@storybook/react';
 
 function loadStories() {
-  // This is some stupid webpack magic. Basically, require in all files in '../stories.'
-  // http://stackoverflow.com/questions/29891458/webpack-require-every-file-in-directory
-  let contextualRequire = require.context('../stories', true, /.*/);
-  contextualRequire.keys().forEach(storybook => {
-    contextualRequire(storybook);
-  });
+  require('../stories');
 }
 
 configure(loadStories, module);
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,38 +1,18 @@
-const path = require('path');
+// you can use this file to add your custom webpack plugins, loaders and anything you like.
+// This is just the basic way to add additional webpack configurations.
+// For more information refer the docs: https://storybook.js.org/configurations/custom-webpack-config
 
-// module.exports = {
-//   module: {
-//     rules: [
-//       {
-//         test: /\.scss$/,
-//         loaders: ["style-loader", "css-loader", "sass-loader"],
-//         include: path.resolve(__dirname, '../')
-//       }
-//     ]
-//   }
-// }
+// IMPORTANT
+// When you add this file, we won't add the default configurations which is similar
+// to "React Create App". This only has babel loader to load JavaScript.
 
-
-// Webpack config
 module.exports = {
+  plugins: [
+    // your custom plugins
+  ],
   module: {
     rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1
-            },
-          },
-          {
-            loader: 'sass-loader',
-          },
-        ],
-        include: path.resolve(__dirname, '../'),
-      },
+      // add your custom rules.
     ],
   },
 };
